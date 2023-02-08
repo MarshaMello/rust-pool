39ad7f30b109bc48469dae861a0e184597c590f6
Hamed Nikbakht <123782996+HamedNikbakht@users.noreply.github.com>
2023-02-08 17:24:39 +0330

    learning-rust

---
+++ first-rust-learning-code/17 - basic-match-expressions-a.rs
@@ -0,0 +1,11 @@
+fn main() {
+    let my_num = 5;
+    match my_name {
+        1 => println!("one"),
+        2 => println!("two"),
+        3 => println!("three"),
+        4 => println!("four"),
+        5 => println!("five"),
+        - => println!("it's not my number"),
+    }
+}
