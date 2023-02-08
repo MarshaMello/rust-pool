f16933827ead09afdc43c3e787d20c4f121be061
Hamed Nikbakht <123782996+HamedNikbakht@users.noreply.github.com>
2023-02-08 17:00:20 +0330

    learning-rust

---
+++ first-rust-learning-code/13 - learning-match.rs
@@ -0,0 +1,10 @@
+// match expression
+// using math expression on adding logic on app
+
+fn main () {
+    let my_bool = true;
+    match my_bool {
+        true => println!("this is true"),
+        false => println!("this is false"),
+    }
+}
---
+++ first-rust-learning-code/14 - learning-match-a.rs
@@ -0,0 +1,9 @@
+fn main() {
+    let some_int = 3;
+    match some_int {
+        1 => println!("it's 1"),
+        2 => println!("it's 2"),
+        3 => println!("it's 3"),
+        _ => println!("it's something else"),
+    }
+}
