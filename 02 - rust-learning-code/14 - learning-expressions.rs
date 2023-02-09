11fbb27c460fdfb73b9f9bcdb8e0a7ff688c83e7
Hamed Nikbakht <123782996+HamedNikbakht@users.noreply.github.com>
2023-02-10 03:05:38 +0330

    learning-rust

---
+++ 02 - rust-learning-code/14 - learning-expressions.rs
@@ -0,0 +1,15 @@
+let my_num = 3;
+let is_lt_5 = if my_num < 5 {
+    true
+} else {
+    false
+};
+
+let is_lt_5 = my_num < 5;
+
+
+let my_num = 3;
+let message = match my_num {
+    1 => "hello",
+    _ => "goodbye"
+};
