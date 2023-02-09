71c1b989d2b47e69a5e8e19556e988e29891b2e1
Hamed Nikbakht <123782996+HamedNikbakht@users.noreply.github.com>
2023-02-10 02:17:15 +0330

    learning-rust

---
+++ 02 - rust-learning-code/10 - working-with-data-tuples.rs
@@ -0,0 +1,14 @@
+ enum Access {
+    Full,
+ }
+
+ fn one_two_three() -> (i32, i32, i32) {
+    (1, 2, 3)
+ }
+
+ let numbers = one_two_three();
+ let (x, y, z) = one_two_three();
+
+ println!("{:?}, {:?}", x, numbers.0);
+ println!("{:?}, {:?}", y, numbers.1);
+ println!("{:?}, {:?}", z, numbers.2);
