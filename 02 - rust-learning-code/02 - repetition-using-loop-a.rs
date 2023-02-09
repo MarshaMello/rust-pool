29a7db410f01c848369e9d63f7be4bea76cf7ba8
Hamed Nikbakht <123782996+HamedNikbakht@users.noreply.github.com>
2023-02-09 20:25:07 +0330

    learning-rust

---
+++ 02 - rust-learning-code/02 - repetition-using-loop-a.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut n = 1;
+    loop {
+        println!("{}:?", n);
+        if n ==4 {
+        break;
+        }
+    }
+    n = n + 1;
+}
---
+++ 02 - rust-learning-code/03 - repetition-using-while.rs
@@ -0,0 +1,9 @@
+// repeat action's multiple time's whithin your program's
+
+fn main() {
+    let mut i = 1;
+    while i <= 3 {
+        println!("{:?}", i);
+        i = i +1;
+    }
+}
