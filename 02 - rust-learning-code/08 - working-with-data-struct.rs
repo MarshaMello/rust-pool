52edd4b10ca58388d7b02dd47410a6fe15d37e3c
Hamed Nikbakht <123782996+HamedNikbakht@users.noreply.github.com>
2023-02-10 02:04:12 +0330

    learning-rust

---
+++ 02 - rust-learning-code/08 - working-with-data-struct.rs
@@ -0,0 +1,13 @@
+struct box_size {
+    depth : i32,
+    width : i32,
+    height : i32,
+}
+let my_box = box_size {
+    depth : 3,
+    width : 2,
+    height : 5,
+}
+
+let tall = my_box.box_size;
+println!("the box is {:?} units tall", tall);
