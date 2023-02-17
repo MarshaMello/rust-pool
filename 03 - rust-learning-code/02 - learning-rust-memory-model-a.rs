ddd6cbd196e3eb44e4e04a1d0c1fed19e8e9d20f
Hamed Nikbakht <123782996+HamedNikbakht@users.noreply.github.com>
2023-02-17 21:52:07 +0330

    learning-rust

---
+++ 03 - rust-learning-code/02 - learning-rust-memory-model-a.rs
@@ -0,0 +1,19 @@
+struct Book {
+    pages: i32,
+    rating: i32,
+}
+
+fn page_count(book: Book) {
+    println!("pages= {:?}, book.pages");
+}
+
+fn book_rating(book: Book) {
+    println!("rating= {:?}, book.rating");
+}
+
+fn main() {
+    let book = Book {
+        pages = 5,
+        rating = 9,
+    };
+}
